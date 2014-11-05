2313c5ba51bf16b9ae1e88e8702ec9a0f7c7300e
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2014-11-05 22:14:28 +0100

    gps without confirmation ?

--- www/js/index.js
+++ www/js/index.js
@@ -54,23 +54,29 @@ onDeviceReady: function() {
 
     if(isMobile)
     {
-    	//gestion GPS
-	/*    var onSuccessGPS = function(position) {
-	       /* alert('Latitude: '        + position.coords.latitude          + '\n' +
-	              'Longitude: '         + position.coords.longitude         + '\n' +
-	              'Altitude: '          + position.coords.altitude          + '\n' +
-	              'Accuracy: '          + position.coords.accuracy          + '\n' +
-	              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-	              'Heading: '           + position.coords.heading           + '\n' +
-	              'Speed: '             + position.coords.speed             + '\n' +
-	              'Timestamp: '         + position.timestamp                + '\n');
-	    };
-	    var onErrorGPS = function(error) {
-	        alert('code: '    + error.code    + '\n' +
-	              'message: ' + error.message + '\n');
-	    };
-	    navigator.geolocation.getCurrentPosition(onSuccessGPS, onErrorGPS);
-        */
+    	// onSuccess Callback
+	// This method accepts a Position object, which contains the
+	// current GPS coordinates
+	//
+	var onSuccess = function(position) {
+	    alert('Latitude: '          + position.coords.latitude          + '\n' +
+		  'Longitude: '         + position.coords.longitude         + '\n' +
+		  'Altitude: '          + position.coords.altitude          + '\n' +
+		  'Accuracy: '          + position.coords.accuracy          + '\n' +
+		  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+		  'Heading: '           + position.coords.heading           + '\n' +
+		  'Speed: '             + position.coords.speed             + '\n' +
+		  'Timestamp: '         + position.timestamp                + '\n');
+	};
+	
+	// onError Callback receives a PositionError object
+	//
+	function onError(error) {
+	    alert('code: '    + error.code    + '\n' +
+		  'message: ' + error.message + '\n');
+	}
+	
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
     /*
     var now                  = new Date().getTime(),
